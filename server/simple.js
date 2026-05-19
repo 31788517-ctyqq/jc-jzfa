99d9e90f7a34a0ca69acf0e363ad0120c7295a75
Sam <31788517@qq.com>
2026-05-20 03:45:57 +0800

    fix: key name compatibility

--- server/simple.js
+++ server/simple.js
@@ -4,14 +4,19 @@ app.use(cors());app.use(express.json());
 app.use('/assets/worldcup',express.static(path.join(__dirname,'../miniprogram/images/worldcup')));
 app.use(express.static(path.join(__dirname,'../preview')));
 
-var data={matches:{},recommends:{}};
-try{data=JSON.parse(fs.readFileSync(path.join(__dirname,'data.json'),'utf8'));console.log('Loaded',Object.keys(data.matches).length,'matches')}catch(e){console.log('No data.json')}
+var data={m:{},r:{}};
+try{
+  var raw=JSON.parse(fs.readFileSync(path.join(__dirname,'data.json'),'utf8'));
+  data.m=raw.m||raw.matches||{};
+  data.r=raw.r||raw.recommends||{};
+  console.log('Loaded',Object.keys(data.m).length,'matches',Object.keys(data.r).length,'recGroups')
+}catch(e){console.log('No data.json:',e.message)}
 
-app.get('/health',function(req,res){res.json({status:'ok',matches:Object.keys(data.matches).length})});
+app.get('/health',function(req,res){res.json({status:'ok',matches:Object.keys(data.m).length})});
 app.post('/api',function(req,res){
   var a=req.body.action,d=req.body.data||{};
-  if(a==='match-list'){return res.json({code:1,data:Object.values(data.matches||{})})}
-  if(a==='match-detail'){var m=data.matches[d.matchId],r=data.recommends[d.matchId]||[];return res.json({code:1,data:{match:m||{},recommends:r}})}
+  if(a==='match-list'){return res.json({code:1,data:Object.values(data.m)})}
+  if(a==='match-detail'){var key='m_'+(d.matchId||''),m=data.m[key]||{},r=data.r[key]||[];return res.json({code:1,data:{match:m,recommends:r}})}
   if(a==='ranking-list'||a==='hit-rate-stats'){return res.json({code:1,data:{categories:{},ranking:[],directionStats:[],totalMatches:0}})}
   res.json({code:0,msg:'Not found'})
 });
